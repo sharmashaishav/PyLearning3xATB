99dde38e1c2e264c333086d2ec46b21a66e8f253
Shaishav Sharma <sharma.shaishav@gmail.com>
2024-06-03 18:19:54 +0530

    Python learning Lab002

---
+++ Ex-20240603/Lab002.py
@@ -0,0 +1,7 @@
+# comment in a line by adding hash at start
+print("First Day of Python Learning")
+print(289031/5)
+print(289031.0011)
+print(289031.)
+print(289031-9281929)
+print("Done for the Day")
