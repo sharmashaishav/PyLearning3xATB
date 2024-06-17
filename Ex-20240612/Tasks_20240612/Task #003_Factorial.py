e9f68cc370a492c01e289fc37262942216cb51a9
Shaishav Sharma <sharma.shaishav@gmail.com>
2024-06-17 08:37:55 +0530

    Python learning

--- Ex-20240612/Tasks_20240612/Task #003_Factorial.py
+++ Ex-20240612/Tasks_20240612/Task #003_Factorial.py
@@ -1,5 +1,13 @@
-
 import math
 
-n = int(input("Enter a number to find its factorial: \n"))
-print(f"Factorial of the given number {n} is :", math.factorial(n))
+# n = int(input("Enter a number to find its factorial: \n"))
+# print(f"Factorial of the given number {n} is :", math.factorial(n))
+
+factorial = 1
+n = 5
+
+for i in range(1, n+1):
+    factorial = factorial * i
+print(factorial)
+
+
