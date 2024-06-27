cb2b4470be0bc1d515096a2d8afd865c99eb1bf7
Shaishav Sharma <sharma.shaishav@gmail.com>
2024-06-28 00:59:49 +0530

    OOPS begin

--- Ex-20240626/Lab031.py
+++ Ex-20240626/Lab031.py
@@ -20,3 +20,25 @@ Shaishav.walk()
 vanya=person()
 vanya.name="Vanya Sharma"
 vanya.talk()
+
+class Dog:
+    name = 'Tommy'
+    id = None
+
+    def sleep(self,name='Tuffy'):
+        print("Name of the Sleeping dog is\n",name)
+
+
+dog1 = Dog()
+print(Dog().name)
+dog1.name = "Chow"
+print(dog1.name)
+dog1.sleep()
+
+print("---------------------")
+
+dog2 = Dog()
+print(Dog().name)
+dog2.name = "Mow"
+print(dog2.name)
+dog2.sleep()
